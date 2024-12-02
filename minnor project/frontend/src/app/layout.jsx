fb1659e659fc60fe70c41b5a34c30738efbf0219
siddharth-sahu-21 <siddharthsahu3333@gmail.com>
2024-12-02 21:58:39 +0530

    add navbar in layout

--- minnor project/frontend/src/app/layout.jsx
+++ minnor project/frontend/src/app/layout.jsx
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "./component/navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +25,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
+       <Navbar />
+       <Toaster position="top-center" /> 
         {children}
       </body>
     </html>
